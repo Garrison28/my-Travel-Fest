cbeb42665c0154f465c8a9e0866bc25d5c2a5b09
Garrison28 <52987934+Garrison28@users.noreply.github.com>
2019-10-17 11:54:55 -0700

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          mag: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'invalid email address'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: function (pendingUser, options, cb) {
+        if (pendingUser && pendingUser.password) {
+          var hash = bcrypt.hashSync(pendingUser.password, 12);
+          pendingUser.password = hash;
+        }
+      }
+    }
+  });
+  user.associate = function (models) {
+    // associations can be defined here
+  };
+  // compares entered password to hashed password
+  user.prototype.validPassword = function(passwordTyped) {
+    return bcrypt.compareSync(passwordTyped, this.password);
+  };
+  // remove the password before serializing
+  user.prototype.toJSON = function() {
+    var userData = this.get();
+    delete userData.password;
+    return userData;
+  };
+
+  return user;
+};
